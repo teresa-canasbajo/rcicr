f6a425503506920b0856e22b9aa879c388ca7dc6
teresa <teresa_canasbajo@berkeley.edu>
2020-09-09 22:01:52 -0700

    trying to force generate reference norms once again

--- R/computeInfoVal2IFC.R
+++ R/computeInfoVal2IFC.R
@@ -86,9 +86,9 @@ computeInfoVal2IFC <- function(target_ci, rdata, random_seed, iter = 10000, forc
   #   }
   # }
   yes <- TRUE
-  if (yes) {#(!exists("ref_median", envir=environment(), inherits=FALSE)) {
+  if (yes == TRUE) {#(!exists("ref_median", envir=environment(), inherits=FALSE)) {
 
-    if (yes) {#(!exists("reference_norms", envir=environment(), inherits=FALSE)) {
+    if (yes == TRUE) {#(!exists("reference_norms", envir=environment(), inherits=FALSE)) {
 
       # Reference norms not present in rdata file, re-generate
       generateReferenceDistribution2IFC(rdata, iter=iter)
@@ -101,7 +101,7 @@ computeInfoVal2IFC <- function(target_ci, rdata, random_seed, iter = 10000, forc
     } else {
 
       write("Using reference distribution found in rdata file.", stdout())
-
+      write("HALLO", stdout())
     }
 
     # Compute reference values
